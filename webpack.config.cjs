4fe068b5375fc0921ea819959da98e0c4ccff4b9
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-02-26 12:09:09 +0500

    updated

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,11 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js' // Chunk hashing
   },
   module: {
     rules: [
@@ -17,8 +18,41 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader', // Optimize images
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: true,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      },
     ]
   },
-  mode: 'production'
+  mode: 'production', // Enable production mode
+  optimization: {
+    minimizer: [
+      new TerserPlugin(), // Minify JavaScript
+    ],
+  },
 };
